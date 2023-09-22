632a72eae6189d6a1ae64d553eb766f483701677
Jhon <117958842+Jhon-Perez@users.noreply.github.com>
2023-09-22 13:53:54 -0500

    Add files via upload

---
+++ critters.py
@@ -0,0 +1,20 @@
+from mod import *
+
+IMAGE = pygame.image.load("Assets/pikachu.png").convert_alpha()
+POKEMON_IMAGE = pygame.transform.scale(IMAGE, (60, 100))
+
+class Critter(pygame.sprite.Sprite):
+    def __init__(self, x, y, health, damage):
+        super().__init__()
+        self.image = POKEMON_IMAGE
+        self.rect = self.image.get_rect()
+
+        self.health = health
+        self.damage = damage
+
+        self.rect.x = x
+        self.rect.y = y
+
+    def draw(self):
+        wn.blit(self.image, (self.rect.x, self.rect.y))
+        
