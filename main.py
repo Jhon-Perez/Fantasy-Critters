c0a5ee790467c192a302b4fe663c6408d7a6e0f9
Brettberry07 <124912529+Brettberry07@users.noreply.github.com>
2023-10-03 22:33:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 from mod import *
 from User import User
 from critters import Critter
+from InteractableObjects import Grass
+from InteractableObjects import Background
 
 pygame.init()
 pygame.display.set_caption("Test")
@@ -10,16 +12,21 @@ clock = pygame.time.Clock()
 FPS = 60
 
 WHITE = (255, 255, 255)
+pygame.display.set_caption("Fantasy Critters")
 
 def main():
     user = User(0, 0)
     critter = Critter(300, 100, 100, 5)
+    grass = Grass(200,200)
+
+    bg = Background("Assets/Basic BG.png",[0,0])
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
         wn.fill(WHITE)
+        wn.blit(bg.image, bg.rect)
 
         critter.draw()
 
