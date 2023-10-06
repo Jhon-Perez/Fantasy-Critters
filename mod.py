e943792b0cfd71fe16cb0a0f2c6f74740bf1ae3d
Brettberry07 <124912529+Brettberry07@users.noreply.github.com>
2023-10-05 21:34:50 -0500

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,6 +1,6 @@
 import pygame
 
 WIDTH = 900
-HEIGHT = 650
+HEIGHT = 560 #may want to test this, my vscode isn't working for some reason
 
 wn = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
