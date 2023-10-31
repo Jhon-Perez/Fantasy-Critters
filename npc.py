b99f035458494975d872d3bce8011f7c2e1e599f
Jhon <117958842+Jhon-Perez@users.noreply.github.com>
2023-10-30 19:04:28 -0500

    Delete npc.py

--- npc.py
+++
@@ -1,7 +0,0 @@
-from mod import *
-
-# TODO - TBD Some random npc probably will be neglected until the
-#  game actually functions properly last thing we'll implement
-class npc(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
