e3fd70cea55b7ebfe92448437c447581a583dc8a
Jhon <117958842+Jhon-Perez@users.noreply.github.com>
2023-10-30 19:04:47 -0500

    Update critters.py

--- critters.py
+++ critters.py
@@ -5,7 +5,7 @@ IMAGE = pygame.image.load("Assets/pikachu.png").convert_alpha()
 POKEMON_IMAGE = pygame.transform.scale(IMAGE, (60, 100))
 
 class Critter(pygame.sprite.Sprite):
-    def __init__(self, x, y, health, damage, name, spriteGroup):
+    def __init__(self, x, y, health, damage, name, score, spriteGroup):
         super().__init__()
         self.image = POKEMON_IMAGE
         self.rect = self.image.get_rect()
@@ -13,6 +13,7 @@ class Critter(pygame.sprite.Sprite):
         self.health = health
         self.damage = damage
         self.name = name
+        self.score = score
 
         self.rect.x = x
         self.rect.y = y
